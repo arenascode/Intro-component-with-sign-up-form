6ce8631b0d6d533690f2855ff60c6c3b566ddbe9
Miguel Arenas <migue.arenas91@gmail.com>
2022-11-12 15:49:29 -0300

    logic validation donde

--- js/main.js
+++ js/main.js
@@ -1,41 +1,64 @@
 const form = document.getElementById("form")
+// form inputs 
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const emailUser = document.getElementById("emailAdress");
+const passUser = document.getElementById("password");
+const errorName = document.getElementById("errorName");
+const errorLastName = document.getElementById("errorLastName");
+const errorMailUser = document.getElementById("errorMail");
+const errorPassUser = document.getElementById("errorPass");
 
-
-  
+firstName.oninput = function (e) {
+  console.log(e);
+  console.log(firstName.value);
+  firstName.classList.remove("imgBG");
+  errorName.innerHTML = ``;
+}
+lastName.oninput = function (e) {
+  console.log(e);
+  console.log(lastName.value);
+  lastName.classList.remove("imgBG");
+  errorLastName.innerHTML = ``;
+};
+emailUser.oninput = function (e) {
+  console.log(e);
+  console.log(emailUser.value);
+  emailUser.classList.remove("imgBG");
+  errorMailUser.innerHTML = ``;
+};
+passUser.oninput = function (e) {
+  console.log(e);
+  console.log(passUser.value);
+  passUser.classList.remove("imgBG");
+  errorPassUser.innerHTML = ``;
+};
+// const deleteErrors = (e) => {
+//   console.log(firstName.value);
+//   firstName.classList.remove("imgBG");
+//   errorName.innerHTML = ``;
+// };
 
 form.onsubmit = (e) => {
   e.preventDefault();
-  const firstName = document.getElementById("firstName");
-  const lastName = document.getElementById("lastName");
-  const emailUser = document.getElementById("emailAdress")
-  const passUser = document.getElementById("password")
-  const errorName = document.getElementById("errorName");
-  const errorLastName = document.getElementById("errorLastName");
-  const errorMailUser = document.getElementById("errorMail");
-  const errorPassUser = document.getElementById("errorPass");
-
-
+  
   const disableFormFields = () => {
     // Name Input
     errorName.innerHTML = `first name cannot be empty`;
     firstName.classList.add("imgBG");
     firstName.removeAttribute("placeholder");
-    firstName.setAttribute("disabled", "disabled")
     // Last Name Input
     errorLastName.innerHTML = `last name cannot be empty`;
     lastName.classList.add("imgBG");
     lastName.removeAttribute("placeholder");
-    lastName.setAttribute("disabled", "disabled");
     // email Input
     errorMailUser.innerHTML = `Looks like this is not an email`;
     emailUser.classList.add("imgBG");
     emailUser.removeAttribute("placeholder");
-    emailUser.setAttribute("disabled", "disabled");
     // pass input
     errorPassUser.innerHTML = `Password cannot be empty`;
     passUser.classList.add("imgBG");
     passUser.removeAttribute("placeholder");
-    passUser.setAttribute("disabled", "disabled");
 
     // let formFields = document.getElementById("form").elements
     // let arrayFields = Array.from(formFields)
@@ -54,24 +77,27 @@ form.onsubmit = (e) => {
     errorName.innerHTML = `first name cannot be empty`;
     firstName.classList.add("imgBG");
     firstName.removeAttribute("placeholder");
-    firstName.setAttribute("disabled", "disabled");
+    firstName.onchange = function () {
+      firstName.classList.remove("imgBG")
+    }
   } else if (lastName.value == "") {
     errorLastName.innerHTML = `last name cannot be empty`;
     lastName.classList.add("imgBG");
     lastName.removeAttribute("placeholder");
-    lastName.setAttribute("disabled", "disabled");
   } else if (emailUser.value == "") {
     errorMailUser.innerHTML = `Looks like this is not an email`;
     emailUser.classList.add("imgBG");
     emailUser.removeAttribute("placeholder");
-    emailUser.setAttribute("disabled", "disabled");
   } else if (passUser.value == "") {
     errorPassUser.innerHTML = `Password cannot be empty`;
     passUser.classList.add("imgBG");
     passUser.removeAttribute("placeholder");
-    passUser.setAttribute("disabled", "disabled");
   } else {
-    errorName.innerHTML = ``;
-    alert("Gracias por completar tus datos")
+    form.reset()
+    firstName.setAttribute("placeholder", "First Name")
+    lastName.setAttribute("placeholder", "Last Name");
+    emailUser.setAttribute("placeholder", "Email Address");
+    firstName.setAttribute("placeholder", "Password");
+    alert("thanks for your information")
 }
 } 
